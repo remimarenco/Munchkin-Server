0b0a958a0e07c1c30573284129c644d762d91a31
Remi Marenco <remi.marenco@gmail.com>
2011-12-06 09:16:53 +0000

    Ajout du constructeur et des commentaires

--- src/carte/Tresor.java
+++ src/carte/Tresor.java
@@ -1,22 +1,22 @@
 package carte;
-import java.util.ArrayList;
-
-import comportement.Action;
 import comportement.classes.Equipement;
 
 
-
+/**
+ * Classe Trésor.
+ * Hérite de la classe Carte.
+ * @author Rémi Marenco
+ */
 public class Tresor extends Carte {
-
-	public Tresor(Equipement equipement) {
-		super();
-		this.equipement = equipement;
-		// TODO Auto-generated constructor stub
+        /**
+         * Constructeur de la classe Tresor.
+         * Enregistre les paramètres de la classe carte ainsi que l'équipement
+         * @param nom
+         * @param description
+         * @param equipement
+         */
+	public Tresor(String nom, String description) {
+            this.nom = nom;
+            this.description = description;
 	}
-
-
-
-
-
-
 }
