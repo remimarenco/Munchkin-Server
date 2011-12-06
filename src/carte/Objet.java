14415e92300d10f3e05871ef2e21ee4cfd4f0b00
Remi Marenco <remi.marenco@gmail.com>
2011-12-06 09:25:54 +0000

    Ajout du constructeur et des commentaires

--- src/carte/Objet.java
+++ src/carte/Objet.java
@@ -5,16 +5,24 @@ import comportement.*;
 import comportement.classes.Equipement;
 
 
-
+/**
+ * Classe Objet.
+ * Hérite de la classe trésor.
+ * Permet de renseigner les objets que les personnages pourront équiper
+ * @author Rémi Marenco
+ */
 public class Objet extends Tresor {
-
-	public Objet(Equipement tab) {
-		super(tab);
-		// TODO Auto-generated constructor stub
-	}
-
-
-
-	
+    /**
+     * Constructeur de la classe objet.
+     * Permet de renseigner les paramètres de la classe Trésor ainsi que l'équipement
+     * apporté par la carte
+     * @param nom
+     * @param description
+     * @param equipement
+     */
+    public Objet(String nom, String description, Equipement equipement) {
+        super(nom, description);
+        this.equipement = equipement;
+    }
 
 }
