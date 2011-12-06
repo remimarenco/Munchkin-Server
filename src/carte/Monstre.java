03c992e5c612e1c9da835c6a590ba7135f2b4d9c
Remi Marenco <remi.marenco@gmail.com>
2011-12-06 09:10:21 +0000

    Ajout du constructeur et des commentaires

--- src/carte/Monstre.java
+++ src/carte/Monstre.java
@@ -3,11 +3,24 @@ package carte;
 import comportement.classes.Condition;
 import comportement.classes.IncidentFacheux;
 
-
+/**
+ * Classe Monstre.
+ * Permet de créer des monstres.
+ * Hérite de la classe Donjon
+ * @author Rémi Marenco
+ */
 public class Monstre extends Donjon {
+    /**
+     * Constructeur de la classe Monstre. Hérite de la classe Donjon
+     * @param nom
+     * @param description
+     * @param condition
+     * @param incidentFacheux
+     */
+    public Monstre(String nom, String description, Condition condition, IncidentFacheux incidentFacheux) {
+        super(nom, description);
+        this.condition = condition;
+        this.incidentFacheux = incidentFacheux;
+    }
 
-	public Monstre(Condition condition, IncidentFacheux tab) {
-		super(tab);
-		// TODO Auto-generated constructor stub
-	}
 }
