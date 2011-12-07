4cbea4996030c5ed3f9cb7d66e4781f7f9b00c38
Remi Marenco <remi.marenco@gmail.com>
2011-12-07 14:17:49 +0000

    Commentaires dans piocherCarte

--- src/comportement/PiocherCarte.java
+++ src/comportement/PiocherCarte.java
@@ -1,18 +1,68 @@
 package comportement;
 
-import joueur.Personnage;
+import joueur.Joueur;
 
+/**
+ * Classe permettant de faire une action de piocher une carte donjon ou trésor
+ * @author Rémi Marenco
+ */
 public class PiocherCarte extends Action {
-    
+
+    /**
+     * Nombre de carte à piocher
+     */
     int nbCarte;
 
-    public PiocherCarte(int nbCarte){
+    /**
+     * Type de pioche dans laquelle piocher les cartes
+     */
+    private int type_pioche;
+
+    /**
+     * Enum du type de pioche
+     */
+    public static final int PIOCHE_TRESOR = 1;
+    public static final int PIOCHE_DONJON = 2;
+
+    /**
+     * Constructeur de piocherCarte
+     * @param type_pioche
+     * => Type de pioche : PIOCHE_TRESOR ou PIOCHE_DONJON
+     * @param nbCarte
+     * => Nombre de cartes à piocher sur l'action
+     */
+    public PiocherCarte(int type_pioche,int nbCarte){
         this.nbCarte = nbCarte;
+        this.type_pioche=type_pioche;
     }
 
+    /**
+     * Action d'une carte qui va faire piocher un joueur dans un type de pioche un nombre de cartes
+     * @param joueurImpacte
+     * => Le joueur qui va piocher les cartes
+     */
     @Override
-    public void action() {
-            // TODO Auto-generated method stub
-            System.out.println("pioche "+nbCarte+" carte");
+    public void action(Joueur joueurImpacte) {
+        // On récupère le nom du joueur qui reçoit l'action
+        System.out.print("Le joueur "+joueurImpacte.getNom());
+        // Affichage selon le nombre de carte
+        if(nbCarte > 1)
+        {
+            System.out.print(" pioche "+nbCarte+" carte dans la pioche ");
+        }
+        else
+        {
+            System.out.print(" pioche "+nbCarte+" cartes dans la pioche ");
+        }
+
+        if(type_pioche == PIOCHE_DONJON)
+        {
+            System.out.println("donjon");
+            joueurImpacte.getMain().ajouterCarte(null);
+        }
+        else
+        {
+            System.out.println("trésor");
+        }
     }
 }
