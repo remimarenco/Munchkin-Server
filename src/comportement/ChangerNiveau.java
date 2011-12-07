68593c3d3ec242670638b9288b9727cd7df59298
Remi Marenco <remi.marenco@gmail.com>
2011-12-07 13:09:34 +0000

    Utilisation de changerNiveau du Personnage du joueur

--- src/comportement/ChangerNiveau.java
+++ src/comportement/ChangerNiveau.java
@@ -1,26 +1,50 @@
 package comportement;
 
+import joueur.Joueur;
+
+/**
+ * Classe permettant de définir l'action d'un changement de niveau
+ * @author Rémi Marenco
+ */
 public class ChangerNiveau extends Action {
-	int niveau;
-	
-	public ChangerNiveau(int niveau)
-	{
-		this.niveau = niveau;
-	}
-	
-	@Override
-	public void action() {
-		// TODO Auto-generated method stub
-		System.out.print("Vous ");
-		if(niveau < 0)
-		{
-			System.out.print("perdez ");
-		}
-		else if(niveau > 0)
-		{
-			System.out.print("gagnez ");
-		}
-		System.out.println(niveau+" niveau !!");
-	}
+
+    /**
+     * Entier permettant de connaître le changement de niveau
+     */
+    int niveau;
+
+    /**
+     * Constructeur de l'action ChangerNiveau
+     * Un niveau positif permet de gagner des niveaux
+     * Un niveau négatif permet de perdre des niveaux
+     * @param niveau
+     */
+    public ChangerNiveau(int niveau)
+    {
+            this.niveau = niveau;
+    }
+
+    /**
+     * Action de ChangerNiveau
+     * Change le niveau selon la variable niveau sur le joueurImpacte
+     * @param joueurImpacte
+     */
+    @Override
+    public void action(Joueur joueurImpacte) {
+            /**
+             * On affiche des informations différentes selon le gain ou la perte de niveau
+             */
+            System.out.println(joueurImpacte.getNom());
+            if(niveau < 0)
+            {
+                    System.out.println(" perds ");
+            }
+            else if(niveau > 0)
+            {
+                    System.out.print("gagne ");
+            }
+            joueurImpacte.getPersonnage().changerNiveau(niveau);
+            System.out.println(niveau+" niveau !!");
+    }
 
 }
