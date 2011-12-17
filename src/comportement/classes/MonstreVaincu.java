d664494825799ffd6f75e36fb5078d91c2eb22c7
Remi Marenco <remi.marenco@gmail.com>
2011-12-17 15:48:55 +0000

    Ajout du comportement monstreVaincu

---
+++ src/comportement/classes/MonstreVaincu.java
@@ -0,0 +1,21 @@
+package comportement.classes;
+
+import java.util.ArrayList;
+
+import joueur.Joueur;
+
+import comportement.Action;
+
+public class MonstreVaincu {
+	public ArrayList<Action> tabAction;
+	
+	public MonstreVaincu(ArrayList<Action> tab)
+    {
+            tabAction = tab;
+    }
+	
+	public void actionMonstreVaincu(Joueur joueurImpacte)
+	{
+		
+	}
+}
