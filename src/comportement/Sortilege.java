11e2ca867d362c9f16820c000ce4ed5b47175d19
Remi Marenco <remi.marenco@gmail.com>
2011-12-23 13:36:45 +0000

    Subversion Eclipse qui chie sur le commit => Obligé un par un

---
+++ src/comportement/Sortilege.java
@@ -0,0 +1,46 @@
+package comportement;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+import action.Action;
+
+
+import joueur.Joueur;
+import joueur.Personnage;
+
+public class Sortilege {
+    private ArrayList<Action> tabAction;
+
+    public Sortilege(ArrayList<Action> tabAction) {
+    	try
+		{
+			this.tabAction = (ArrayList<Action>) tabAction.clone();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Aucune action dans le sort");
+		}
+    }
+
+    public String actionSortilege(Joueur joueurImpacte)
+    {
+        String out = "";
+        if(tabAction == null)
+        {
+            out += "Aucune sortilège à appliquer";
+        }
+        else
+        {
+            out += "----- Sortilège -----\n";
+            out += "Un sortilège s'applique sur " + joueurImpacte.getName() + " :";
+            for(Action action : tabAction)
+                out += action.action(joueurImpacte);
+            out += "\n";
+            out += "--- Fin sortilège ---";
+        }
+        out += "\n";
+
+        return out;
+    }
+}
