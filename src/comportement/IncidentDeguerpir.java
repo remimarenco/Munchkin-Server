c5bd77f702fa033024f9e51af372ef15849bf75c
Remi Marenco <remi.marenco@gmail.com>
2011-12-23 13:36:28 +0000

    Subversion Eclipse qui chie sur le commit => Obligé un par un

---
+++ src/comportement/IncidentDeguerpir.java
@@ -0,0 +1,45 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package comportement;
+
+import java.util.ArrayList;
+
+import action.Action;
+import joueur.Joueur;
+
+
+/**
+ *
+ * @author simon.grabit
+ */
+public class IncidentDeguerpir {
+
+        public ArrayList<Action> tabAction;
+
+	public IncidentDeguerpir(ArrayList<Action> tabAction)
+        {
+			try
+			{
+				this.tabAction = (ArrayList<Action>) tabAction.clone();
+			}
+			catch(Exception e)
+			{
+				System.out.println("Aucune action dans l'incident deguerpir");
+			}
+        }
+
+        public String actionIncidentDeguerpir(Joueur joueurImpacte)
+	{
+            String out = "";
+            out += "--- Incident de fuite ---\n";
+            out += "Le joueur a fuit mais a subit l'incident suivant " + joueurImpacte.getName() + " :\n";
+            for(Action action : tabAction)
+            {
+                    out += action.action(joueurImpacte);
+            }
+            out += "--- Fin de monstre vaincu ---\n";
+            return out;
+        }
+}
