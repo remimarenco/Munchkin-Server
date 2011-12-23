b99c1c8455cbf9fe9d75491bc4e54bc91e8e1447
Remi Marenco <remi.marenco@gmail.com>
2011-12-23 13:36:22 +0000

    Subversion Eclipse qui chie sur le commit => Obligé un par un

---
+++ src/comportement/Equipement.java
@@ -0,0 +1,52 @@
+package comportement;
+
+import java.util.ArrayList;
+import java.util.Vector;
+
+import action.Action;
+
+
+import joueur.Joueur;
+import joueur.Personnage;
+
+
+
+public  class Equipement {
+	private ArrayList<Action> tabAction;
+	
+	
+	
+	public Equipement(ArrayList<Action> tabAction) {
+		try
+		{
+			this.tabAction = (ArrayList<Action>) tabAction.clone();
+		}
+		catch(Exception e)
+		{
+			System.out.println("Aucune action dans l'equipement");
+		}
+	}
+
+
+
+	public String equipe(Joueur joueurImpacte)
+        {
+            String out = "";
+
+            if(tabAction != null)
+            {
+                out += "--- Equipement ---\n";
+                out += "Un equipement vient de se déclencher sur " + joueurImpacte.getName() + " :\n";
+                for(Action action : tabAction)
+                    out += action.action(joueurImpacte);
+                out += "--- Fin d'equipement ---\n";
+            }
+            else
+            {
+               out += "Aucun equipement !\n";
+            }
+
+            return out;
+	}
+	
+}
